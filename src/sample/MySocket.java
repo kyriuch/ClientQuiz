8e0b64f1044ca4cf87056c7159f280ce20fe980e
kyriuch <kyriuch@gmail.com>
2017-04-26 00:00:31 +0200

    Totally switched to object streams.

--- src/sample/MySocket.java
+++ src/sample/MySocket.java
@@ -5,21 +5,15 @@ import org.slf4j.LoggerFactory;
 
 import java.io.*;
 import java.net.Socket;
+
 public class MySocket extends Socket implements Runnable {
 
-    private PrintWriter printWriter;
+    private ObjectOutputStream objectOutputStream;
     private ObjectInputStream objectInputStream;
     private boolean isRunning;
 
     MySocket(String host, int port) throws IOException {
         super(host, port);
-    }
-
-    private void proceedIncomingTcpMessage(TcpMessage tcpMessage) throws IOException {
-        if(tcpMessage.getOutType().equals(Question.class.getName())) {
-            Question question = (Question) tcpMessage.getOutObject();
-            Main.controller.setCurrentQuestion(question.getContent());
-        }
     }
 
     @Override
@@ -30,14 +24,14 @@ public class MySocket extends Socket implements Runnable {
         logger.info("Starting socket - " + String.valueOf(this));
 
         try {
-            printWriter = new PrintWriter(getOutputStream(), true);
+            objectOutputStream = new ObjectOutputStream(getOutputStream());
             objectInputStream = new ObjectInputStream(getInputStream());
         } catch (IOException e) {
             isRunning = false;
             e.printStackTrace();
         }
 
-        while(isRunning) {
+        while (isRunning) {
             try {
                 TcpMessage tcpMessage = (TcpMessage) objectInputStream.readObject();
                 logger.info("Got TcpMessage - " + String.valueOf(tcpMessage));
@@ -57,6 +51,18 @@ public class MySocket extends Socket implements Runnable {
     public void stop() throws IOException {
         isRunning = false;
         objectInputStream.close();
-        printWriter.close();
+        objectOutputStream.close();
+    }
+
+    private void proceedIncomingTcpMessage(final TcpMessage tcpMessage) throws IOException {
+        Logger logger = LoggerFactory.getLogger(MySocket.class);
+
+        if (tcpMessage.getOutClass().getSimpleName().equals(Question.class.getSimpleName())) {
+            tcpMessage.setHandler((obj) -> Main.controller.setCurrentQuestion(((Question) obj).getContent()));
+        } else if (tcpMessage.getOutClass().getSimpleName().equals(ChatMessage.class.getSimpleName())) {
+            tcpMessage.setHandler((obj) -> logger.info(obj.toString()));
+        }
+
+            tcpMessage.executeHandler();
     }
 }
